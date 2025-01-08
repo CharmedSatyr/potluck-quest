95975524338dbb540b12e5b7eb6d06e6ec4589b4
Joseph Wolfe <joseph@WLF.to>
2025-01-08 13:08:43 -0800

    Rename eslint repo namespace

--- apps/bot/eslint.config.mjs
+++ apps/bot/eslint.config.mjs
@@ -1,4 +1,4 @@
-import baseConfig from "@repo/eslint-config/base";
+import baseConfig from "@potluck/eslint-config/base";
 import tsParser from "@typescript-eslint/parser";
 
 /** @type {import("eslint").Linter.Config[]} */
--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,14 +1,14 @@
-import baseConfig from "@repo/eslint-config/base";
-import nextConfig from "eslint-config-next/core-web-vitals.js";
+import baseConfig from "@potluck/eslint-config/base";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
 import pluginDrizzle from "eslint-plugin-drizzle";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
 	...baseConfig,
 	{
-		plugins: nextConfig.extends.plugins,
-		rules: nextConfig.extends.rules,
-		overrides: nextConfig.extends.overrides,
+		plugins: nextCoreWebVitals.extends.plugins,
+		rules: nextCoreWebVitals.extends.rules,
+		overrides: nextCoreWebVitals.extends.overrides,
 	},
 	{
 		plugins: {
