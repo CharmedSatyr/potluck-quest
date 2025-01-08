b6f15246e7c18d4786a6a28ad2cf5681ae7b448f
Joseph Wolfe <joseph@WLF.to>
2025-01-08 15:37:01 -0800

    Fix web eslint config

--- packages/eslint-config/web.mjs
+++ packages/eslint-config/web.mjs
@@ -1,18 +1,22 @@
-import baseConfig from "./base.mjs"
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
+import { FlatCompat } from "@eslint/eslintrc";
 import pluginDrizzle from "eslint-plugin-drizzle";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import baseConfig from "./base.mjs"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
 	...baseConfig,
-	{
-		plugins: nextCoreWebVitals.extends.plugins,
-		rules: nextCoreWebVitals.extends.rules,
-		overrides: nextCoreWebVitals.extends.overrides,
-	},
+	...compat.extends("next/core-web-vitals"),
 	{
 		plugins: {
-			drizzle: pluginDrizzle,
+			drizzle: pluginDrizzle
 		},
 	},
 ];
