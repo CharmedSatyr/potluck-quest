c104848b8d50cd3f53da7d884d9a4b1f83c9e575
Joseph Wolfe <joseph@WLF.to>
2025-01-09 08:39:57 -0800

    Fix config key

--- apps/bot/src/config.ts
+++ apps/bot/src/config.ts
@@ -13,7 +13,7 @@ export default {
 	CLIENT_ID: getEnvVar("CLIENT_ID"),
 	PORT: getEnvVar("PORT"),
 	PUBLIC_KEY: getEnvVar("PUBLIC_KEY"),
-	PQ_BOT_TO_WEB_API_KEY: getEnvVar("BOT_TO_WEB_API_KEY"),
-	PQ_WEB_TO_BOT_API_KEY: getEnvVar("WEB_TO_BOT_API_KEY"),
+	PQ_BOT_TO_WEB_API_KEY: getEnvVar("PQ_BOT_TO_WEB_API_KEY"),
+	PQ_WEB_TO_BOT_API_KEY: getEnvVar("PQ_WEB_TO_BOT_API_KEY"),
 	PQ_BASE_URL: getEnvVar("PQ_BASE_URL"),
 };
