c6489403a668fe1bba8bd79e75264633e6e1251f
Joseph Wolfe <joseph@WLF.to>
2025-01-09 08:57:45 -0800

    Restore config values

--- apps/web/src/db/config.ts
+++ apps/web/src/db/config.ts
@@ -1,4 +1,16 @@
+const {
+	DATABASE_HOST,
+	DATABASE_NAME,
+	DATABASE_PASSWORD,
+	DATABASE_URL,
+	DATABASE_USER,
+} = process.env;
+
 export const config = {
-	connectionString: process.env.DATABASE_URL,
+	connectionString: DATABASE_URL!,
+	database: DATABASE_NAME!,
+	host: DATABASE_HOST!,
+	password: DATABASE_PASSWORD!,
 	ssl: true,
+	user: DATABASE_USER!,
 };
