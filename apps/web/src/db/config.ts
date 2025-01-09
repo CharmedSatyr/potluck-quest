eea645e05253f198af6db2b737085f7cceb06667
Joseph Wolfe <joseph@WLF.to>
2025-01-09 08:50:50 -0800

    Remove unused values

--- apps/web/src/db/config.ts
+++ apps/web/src/db/config.ts
@@ -1,16 +1,4 @@
-const {
-	DATABASE_HOST,
-	DATABASE_NAME,
-	DATABASE_PASSWORD,
-	DATABASE_URL,
-	DATABASE_USER,
-} = process.env;
-
 export const config = {
-	connectionString: DATABASE_URL!,
-	database: DATABASE_NAME!,
-	host: DATABASE_HOST!,
-	password: DATABASE_PASSWORD!,
+	connectionString: process.env.DATABASE_URL,
 	ssl: true,
-	user: DATABASE_USER!,
 };
