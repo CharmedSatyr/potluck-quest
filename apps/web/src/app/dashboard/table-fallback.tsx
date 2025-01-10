4c3d2cc570927409a31ab6cd8f0607e18a4ffe98
Joseph Wolfe <joseph@WLF.to>
2025-01-10 13:40:42 -0800

    Simplify and improve table fallback

--- apps/web/src/app/dashboard/table-fallback.tsx
+++ apps/web/src/app/dashboard/table-fallback.tsx
@@ -1,20 +1,5 @@
 export const TableFallback = () => (
-	<div className="flex w-full flex-col gap-4">
-		<div className="skeleton h-12 w-1/4" />
-		<div className="flex justify-around gap-2">
-			<div className="skeleton h-12 w-2/3" />
-			<div className="skeleton h-12 w-1/6" />
-			<div className="skeleton h-12 w-1/6" />
-		</div>
-		<div className="flex justify-around gap-2">
-			<div className="skeleton h-12 w-2/3" />
-			<div className="skeleton h-12 w-1/6" />
-			<div className="skeleton h-12 w-1/6" />
-		</div>
-		<div className="flex justify-around gap-2">
-			<div className="skeleton h-12 w-2/3" />
-			<div className="skeleton h-12 w-1/6" />
-			<div className="skeleton h-12 w-1/6" />
-		</div>
+	<div className="flex w-full flex-col gap-16">
+		<div className="skeleton h-48 w-full" />
 	</div>
 );
