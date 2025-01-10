44be2f1b38fd573b90684a9ba2b1f3bfeb0523b8
Joseph Wolfe <joseph@WLF.to>
2025-01-10 15:30:35 -0800

    Update settings loading component

--- apps/web/src/app/settings/loading.tsx
+++ apps/web/src/app/settings/loading.tsx
@@ -1,8 +1,11 @@
 const Loading = () => (
-	<div className="mb-2 flex w-full flex-col gap-4">
-		<div className="skeleton h-12 w-1/3 md:h-16" />
-		<div className="skeleton h-8 w-3/4" />
-		<div className="skeleton h-8 w-1/2" />
+	<div className="mb-2 flex w-full flex-col gap-8">
+		<div className="skeleton h-12 w-1/3 md:h-10" />
+
+		<div className="-m-4 flex flex-col gap-4 rounded-xl bg-base-100 p-4">
+			<div className="skeleton h-10 w-3/4" />
+			<div className="skeleton h-10 w-1/2" />
+		</div>
 	</div>
 );
 
