84835c788c0f03f1d675b332a6855113a96673e7
Joseph Wolfe <joseph@WLF.to>
2025-01-15 13:00:43 -0800

    Send the status response

--- apps/bot/src/utilities/verify-api-key-middleware.ts
+++ apps/bot/src/utilities/verify-api-key-middleware.ts
@@ -7,7 +7,7 @@ const verifyApiKeyMiddleware = (
 	next: NextFunction
 ) => {
 	if (req.headers["x-api-key"] !== envConfig.PQ_WEB_TO_BOT_API_KEY) {
-		res.status(401);
+		res.status(401).send();
 		return;
 	}
 
