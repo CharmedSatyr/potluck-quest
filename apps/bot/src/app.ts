1ca6b50de8459b0f84a7fd22e10b416282e15a60
Joseph Wolfe <joseph@WLF.to>
2025-01-16 15:34:57 -0800

    Add user routes

--- apps/bot/src/app.ts
+++ apps/bot/src/app.ts
@@ -1,10 +1,12 @@
 import verifyApiKeyMiddleware from "./utilities/verify-api-key-middleware";
 import express from "express";
 import eventRouter from "~/routes/event";
+import userRouter from "~/routes/user";
 
 const app = express();
 
 app.use(verifyApiKeyMiddleware);
 app.use("/api/event", eventRouter);
+app.use("/api/user", userRouter);
 
 export default app;
---
+++ apps/bot/src/routes/user.ts
@@ -0,0 +1,40 @@
+import { Router, Request, Response } from "express";
+import client from "~/client";
+
+const router = Router();
+
+router.get("/guilds", async (req: Request, res: Response): Promise<void> => {
+	const { query } = req;
+	const { userId } = query;
+
+	// TODO: zod
+	if (typeof userId !== "string") {
+		res.status(400);
+		return;
+	}
+
+	const botGuilds = client.guilds.cache;
+	const guildsWithPermissions = [];
+
+	for (const [guildId, guild] of botGuilds) {
+		const member = await guild.members.fetch(userId).catch(() => null);
+
+		if (!member) {
+			continue;
+		}
+
+		if (member.permissions.has("ManageEvents")) {
+			guildsWithPermissions.push({
+				name: guild.name,
+				guildId,
+				iconURL: guild.iconURL(),
+			});
+		}
+	}
+
+	res.json({
+		guilds: guildsWithPermissions,
+	});
+});
+
+export default router;
