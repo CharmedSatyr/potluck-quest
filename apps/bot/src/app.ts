902acc2b43675089c20a8e914da61e5fadad5c57
Joseph Wolfe <joseph@WLF.to>
2025-01-17 04:04:06 -0800

    Move middleware to new folder

--- apps/bot/src/app.ts
+++ apps/bot/src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
+import verifyApiKey from "~/middleware/verify-api-key";
 import eventRouter from "~/routes/event";
 import userRouter from "~/routes/user";
-import verifyApiKeyMiddleware from "~/utilities/verify-api-key-middleware";
 
 const app = express();
 
 app.use(express.json());
-app.use(verifyApiKeyMiddleware);
+app.use(verifyApiKey);
 app.use("/api/event", eventRouter);
 app.use("/api/user", userRouter);
 
