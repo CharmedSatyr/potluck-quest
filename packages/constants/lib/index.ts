d3717850282916497822a8cafa57715d56da14ec
Joseph Wolfe <joseph@WLF.to>
2025-01-17 11:59:52 -0800

    Build out shared constants package

---
+++ packages/constants/lib/index.ts
@@ -0,0 +1,15 @@
+import { SupportedTimezone, SupportedTimezones } from "@potluck/types";
+
+export const DESCRIPTION_LENGTH = 500;
+export const EVENT_CODE_LENGTH = 5;
+export const HOSTS_LENGTH = 100;
+export const DEFAULT_TIMEZONE: SupportedTimezone = "America/Los_Angeles";
+
+export const SUPPORTED_TIMEZONES: SupportedTimezones = [
+  "Pacific/Honolulu",
+  "America/Anchorage",
+  "America/Los_Angeles",
+  "America/Denver",
+  "America/Chicago",
+  "America/New_York",
+] as const;
