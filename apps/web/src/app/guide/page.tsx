eccf90229b37b0332895d48af370052e90c550ec
Joseph Wolfe <joseph@WLF.to>
2025-01-29 19:36:44 -0800

    Additional work on guide text

--- apps/web/src/app/guide/page.tsx
+++ apps/web/src/app/guide/page.tsx
@@ -34,9 +34,9 @@ const WithBotContent = () => {
 				Using <PotluckQuest /> with <PQBot /> on <DiscordLogo width={110} />
 			</h2>
 			<p>
-				<PQBot />, our Discord bot, allows you to plan and manage events
-				entirely within Discord, with the <PotluckQuest /> web interface
-				available for slot creation and extra features.
+				<PQBot /> allows you to plan and manage events entirely within{" "}
+				<DiscordLogo />, with the <PotluckQuest /> web interface available for
+				slot creation and extra features.
 			</p>
 			<p>
 				<PQBot /> is designed for{" "}
@@ -67,12 +67,11 @@ const WithBotContent = () => {
 					</li>
 					<li>
 						Once <SignupSlots /> are ready, guests can type{" "}
-						<Command command="slots" /> followed by the event code in Discord
-						and click on what they&apos;d like to bring.
+						<Command command="slots" /> followed by the event code in{" "}
+						<DiscordLogo /> and click on what they&apos;d like to bring.
 					</li>
 					<li>
-						The event will function and can be managed like any other Discord
-						event.
+						The event can be managed like any other <DiscordLogo /> event.
 					</li>
 				</ol>
 			</article>
@@ -89,8 +88,8 @@ const WithBotContent = () => {
 						Discord Scheduled Events
 					</Link>{" "}
 					that are linked to unique, five-character, alphanumeric codes on the{" "}
-					<PotluckQuest /> website. You can manage events from the website or
-					Discord, and the details will remain synced.
+					<PotluckQuest /> website. You can manage events from the website or{" "}
+					<DiscordLogo />, and the details will remain synced.
 				</p>
 
 				<section>
@@ -102,21 +101,21 @@ const WithBotContent = () => {
 							set on the <Link href="/settings">Settings</Link> page.
 						</li>
 						<li>
-							<PQBot /> will create events on <PotluckQuest /> and Discord and
-							announce the event in the channel in which it was created. It will
-							also add the event&apos;s code to the event description for easy
-							access.
+							<PQBot /> will create events on <PotluckQuest /> and{" "}
+							<DiscordLogo /> and announce the event in the channel in which it
+							was created. It will also add the event&apos;s code to the event
+							description for easy access.
 						</li>{" "}
 						<li>
 							If you&apos;d rather use the <PotluckQuest /> web interface to
 							create an event, you can associate your event with any server that
 							you are allowed to create an event in and that has <PQBot />{" "}
 							installed. Simply select the server you want linked in the last
-							step of the planning wizard.
+							step of the <Link href="/plan">planning wizard</Link>.
 						</li>
 						<li>
-							Any events associated with a Discord server will be visible{" "}
-							<strong>only to members of that server</strong> on the{" "}
+							Any events associated with a <DiscordLogo /> server will be
+							visible <strong>only to members of that server</strong> on the{" "}
 							<PotluckQuest /> web interface.
 						</li>
 						<li>
@@ -124,8 +123,8 @@ const WithBotContent = () => {
 							it&apos;s been created.
 						</li>
 						<li>
-							Some Discord event functionality, like cover images and repeating
-							events, is not yet available on <PotluckQuest />.
+							Some <DiscordLogo /> event functionality, like cover images and
+							repeating events, is not yet available on <PotluckQuest />.
 						</li>
 					</ul>
 				</section>
@@ -135,8 +134,8 @@ const WithBotContent = () => {
 					<ul>
 						<li>
 							Use <Command command="view" /> to see all <PotluckQuest /> events
-							on your Discord server, along with their codes and links to their
-							pages on the website.
+							on your <DiscordLogo /> server, along with their codes and links
+							to their pages on the website.
 						</li>
 						<li>
 							You can also visit your <Link href="/dashboard">Dashboard</Link>{" "}
@@ -164,12 +163,9 @@ const WithBotContent = () => {
 							<Link href="/dashboard">Dashboard</Link>.
 						</li>
 						<li>
-							Events modified on Discord will be permanently changed on the{" "}
-							<PotluckQuest /> website, and vice versa.
-						</li>
-						<li>
-							Events canceled on Discord will be permanently deleted on the{" "}
-							<PotluckQuest /> website, and vice versa.
+							Events modified or canceled on <DiscordLogo /> will be permanently
+							changed or deleted on the <PotluckQuest /> website, and vice
+							versa.
 						</li>
 					</ul>
 				</section>
@@ -178,21 +174,26 @@ const WithBotContent = () => {
 			<article>
 				<h3>Using Slots</h3>
 				<p>
-					Signup slots are central to the functionality of <PotluckQuest />.
+					Signup slots are how <PotluckQuest /> helps you plan meals. A slot
+					represents a request for a specific type and number of item—e.g., main
+					course, appetizers, drinks, or packages of paper plates.{" "}
+					<OpenAiLogo /> can help you determine what you need to support the
+					number of attendees you expect at your event.
 				</p>
 
 				<section>
 					<h4>Creating Slots</h4>
 					<ul>
 						<li>
-							Use the web interface to create and customize signup slots.{" "}
-							<OpenAiLogo /> can suggest dish types and quantities based on your
-							event details and the number of attendees.
+							The <Link href="/plan">planning wizard</Link> of <PotluckQuest />{" "}
+							is where you create and customize signup slots. <OpenAiLogo /> can
+							suggest dish types and quantities based on your event details and
+							the number of attendees.
 						</li>
 						<li>
-							Use of <OpenAiLogo /> suggestions is completely optional, and only
-							information you choose to include in your event details is sent
-							when requesting slot suggestions.
+							Use of <OpenAiLogo /> suggestions is completely optional. The only
+							information shared is the number of attendees and the details of
+							the event you're planning.
 						</li>
 					</ul>
 				</section>
@@ -201,12 +202,12 @@ const WithBotContent = () => {
 					<h4>Signing Up for Slots</h4>
 					<ul>
 						<li>
-							<strong>Discord:</strong> Use <code>/slots</code> to view
-							available slots and sign up without leaving Discord.
+							Use <code>/slots</code> in your <DiscordLogo /> server to view
+							available slots and sign up.
 						</li>
 						<li>
-							<strong>Website:</strong> Participants can also sign up for slots
-							directly via the event link.
+							Participants can also sign up for slots directly on the{" "}
+							<PotluckQuest /> web interface via the event page.
 						</li>
 					</ul>
 				</section>
@@ -214,13 +215,17 @@ const WithBotContent = () => {
 
 			<article>
 				<h3>RSVPs</h3>
-				<p>
-					Participants can RSVP within Discord by clicking
-					&quot;Interested&quot; on the native event. This status is synced to
-					the <PotluckQuest /> web interface. On the web interface, RSVPs can be
-					managed by clicking &quot;Attending&quot; or &quot;Not Attending&quot;
-					on the event page.
-				</p>
+				<ul>
+					<li>
+						Participants can RSVP within <DiscordLogo /> by clicking
+						&quot;Interested&quot; on the native event. This status is synced to
+						the <PotluckQuest /> web interface.
+					</li>
+					<li>On the web interface, RSVPs can be
+						managed by clicking &quot;Attending&quot; or &quot;Not Attending&quot;
+						on the event page. This information will be synced to the connected <DiscordLogo /> event.
+					</li>
+				</ul>
 			</article>
 
 			<article>
@@ -239,7 +244,7 @@ const WithBotContent = () => {
 					</li>
 				</ul>
 			</article>
-		</section>
+		</section >
 	);
 };
 
@@ -383,68 +388,53 @@ const GuideContent = () => {
 				<span className="text-primary-gradient font-bold">
 					<PotluckQuest />
 				</span>{" "}
-				makes potluck planning simple and collaborative, whether you&apos;re
-				taking full advantage of our powerful <DiscordLogo /> integration with{" "}
+				and its <OpenAiLogo />
+				-assisted tools make shared meal planning simple and collaborative,
+				whether you&apos;re taking full advantage of our powerful{" "}
+				<DiscordLogo /> integration with{" "}
 				<span className="text-primary-gradient font-bold">
 					<PQBot />
 				</span>{" "}
-				or prefer the standalone web interface and <OpenAiLogo />
-				-assisted tools.
+				or prefer the standalone web interface.
 			</p>
 
 			<SyncOptionTabsWrapper
 				withSyncContent={<WithBotContent />}
 				withoutSyncContent={<WebOnlyContent />}
 			/>
-
-			<footer>
-				<p>
-					<PotluckQuest /> makes event planning flexible and accessible, whether
-					you prefer the Discord bot or the standalone web interface. Start your
-					potluck today!
-				</p>
-			</footer>
 		</main>
 	);
 };
 const Page = () => {
-	// Add a drawer menu that shows what's on this page.
-	return (
-		<>
+	return <div className="drawer lg:drawer-open">
+		<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+		<div className="drawer-content flex flex-col items-center justify-center">
 			<GuideContent />
+			<label
+				htmlFor="my-drawer-2"
+				className="btn btn-primary drawer-button lg:hidden"
+			>
+				Open drawer
+			</label>
+		</div >
 
-			{/*
-            <div className="drawer lg:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
-                    <GuideContent />
-            <label
-                htmlFor="my-drawer-2"
-                className="btn btn-primary drawer-button lg:hidden"
-            >
-                Open drawer
-            </label>
-        </div >
+		<div className="drawer-side">
+			<label
+				htmlFor="my-drawer-2"
+				aria-label="close sidebar"
+				className="drawer-overlay"
+			></label>
+			<ul className="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
+				<li>
+					<a>Sidebar Item 1</a>
+				</li>
+				<li>
+					<a>Sidebar Item 2</a>
+				</li>
+			</ul >
+		</div>
+	</div>
 
-
-            <div className="drawer-side">
-                <label
-                    htmlFor="my-drawer-2"
-                    aria-label="close sidebar"
-                    className="drawer-overlay"
-                ></label>
-                <ul className="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
-            <li>
-                <a>Sidebar Item 1</a>
-            </li>
-            <li>
-                <a>Sidebar Item 2</a>
-            </li>
-        </ul >
-            </div >
-            */}
-		</>
-	);
-};
+}
 
 export default Page;
