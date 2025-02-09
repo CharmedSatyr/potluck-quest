6ba0c6cd8e17d194c34ba66796eb78a48bab9178
Joseph Wolfe <joseph@WLF.to>
2025-02-09 11:09:18 -0800

    Add potluck daisyui theme to globals.css

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,5 +1,4 @@
 import typography from "@tailwindcss/typography";
-//import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -17,27 +16,6 @@ const config: Config = {
 			},
 		},
 	},
-	/*
-	daisyui: {
-		themes: [
-			{
-				potluck: {
-					primary: "#FF8A50", // Slightly muted orange for vibrancy on dark
-					secondary: "#4CAF50", // Deep green with good contrast
-					accent: "#FFB74D", // Rich golden-yellow for highlights
-					neutral: "#3E2723", // Dark brown for a warm neutral base
-					"base-100": "hsl(229deg 25% 14%)",
-					"base-200": "hsl(229deg 25% 8%)",
-					"base-300": "hsl(229deg 25% 4%)",
-					info: "#1E88E5", // Deep blue for informational tones
-					success: "#66BB6A", // Calm green for success indicators
-					warning: "#FFB300", // Bold amber for warnings
-					error: "#D32F2F", // Deep red for errors
-				},
-			},
-		],
-	},
-	*/
-	plugins: [typography /*daisyui*/],
+	plugins: [typography],
 };
 export default config;
