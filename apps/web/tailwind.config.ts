c207ca64352005395f4984f5e16f3f28c1187180
Joseph Wolfe <joseph@WLF.to>
2025-02-09 11:13:24 -0800

    Define blurple with css config; fix accent hex

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -8,14 +8,7 @@ const config: Config = {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/data/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	theme: {
-		extend: {
-			colors: {
-				blurple: "#5865F2", // Official Discord blur/purple
-				"dark-blurple": "#4955CC", // Custom on hover Discord blurple hue
-			},
-		},
-	},
 	plugins: [typography],
 };
+
 export default config;
