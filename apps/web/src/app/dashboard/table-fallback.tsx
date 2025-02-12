0213fc65281579c8ffd8d40053d2cee2eb0913c6
Joseph Wolfe <joseph@WLF.to>
2025-02-12 10:20:24 -0800

    Restyling dashboard

--- apps/web/src/app/dashboard/table-fallback.tsx
+++ apps/web/src/app/dashboard/table-fallback.tsx
@@ -1,6 +1,6 @@
 export const TableFallback = () => (
-	<div className="border-base-300 overflow-x-auto rounded-xl border shadow-sm">
-		<table className="table-pin-rows table-sm bg-base-100 my-0 table rounded-xl">
+	<div className="border-base-300 bg-base-100 overflow-x-auto rounded-xl border shadow-sm">
+		<table className="table-sm mt-0 mb-0 table table-auto">
 			<thead>
 				<tr>
 					<th>Code</th>
@@ -11,8 +11,8 @@ export const TableFallback = () => (
 			</thead>
 			<tbody>
 				<tr>
-					<td>
-						<div className="skeleton h-8" />
+					<td className="w-22">
+						<div className="skeleton h-8 w-16" />
 					</td>
 					<td>
 						<div className="skeleton h-8" />
@@ -25,8 +25,8 @@ export const TableFallback = () => (
 					</td>
 				</tr>
 				<tr>
-					<td>
-						<div className="skeleton h-8" />
+					<td className="w-22">
+						<div className="skeleton h-8 w-16" />
 					</td>
 					<td>
 						<div className="skeleton h-8" />
@@ -39,8 +39,8 @@ export const TableFallback = () => (
 					</td>
 				</tr>
 				<tr>
-					<td>
-						<div className="skeleton h-8" />
+					<td className="w-22">
+						<div className="skeleton h-8 w-16" />
 					</td>
 					<td>
 						<div className="skeleton h-8" />
