6f5e81cd8e221da45f87defd38493644431235fe
Joseph Wolfe <joseph@WLF.to>
2025-03-02 09:48:07 -0800

    Build business address in the client

---
+++ apps/web/src/components/business-address.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const BusinessAddress = () => {
+    const address = "62189 AW ,elttaeS ,WS evA ht03 6357".split("").reverse().join("")
+
+    return <span>{address}</span>
+}
+
+export default BusinessAddress;
