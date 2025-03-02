ed3632e070d7b5874367f827fc93a26411bcb8b7
Joseph Wolfe <joseph@WLF.to>
2025-03-01 16:46:41 -0800

    Styling updates to match slot container better

--- apps/web/src/components/current-menu-list.tsx
+++ apps/web/src/components/current-menu-list.tsx
@@ -1,30 +1,5 @@
+import OverlappingAvatars from "./overlapping-avatars";
 import findSlotContainerDetails from "~/actions/slot/find-slot-container-details";
-
-//import UserAvatar from "~/components/user-avatar";
-
-/*
-type Users = {
-	id: string;
-	image: string | null;
-	name: string | null;
-	commitments: number;
-}[];
-
-const Avatars = ({ users }: { users: Users }) => {
-	return users.map(({ id, image, name, commitments }) =>
-		image ? (
-			<div key={id} className="indicator not-prose">
-				<span className="indicator-item badge badge-primary badge-xs">
-					{commitments}
-				</span>
-				<UserAvatar name={name} url={image} height={30} width={30} />
-			</div>
-		) : (
-			<div key={id} className="skeleton h-8 w-8 rounded-full border" />
-		)
-	);
-};
-*/
 
 const CurrentMenuList = async ({ code }: { code: string }) => {
 	const slotContainerDetails = await findSlotContainerDetails({ code });
@@ -36,12 +11,15 @@ const CurrentMenuList = async ({ code }: { code: string }) => {
 	return (
 		<ul className="list bg-base-100 rounded-box not-prose border-base-300 border shadow-md">
 			{slotContainerDetails.map((detail) => (
-				<li key={detail.slotId} className="list-row items-center">
-					<div className="list-col-grow">
-						<div>{detail.item}</div>
+				<li
+					key={detail.slotId}
+					className="list-row flex items-center justify-between"
+				>
+					<div className="list-col-grow text-sm md:w-6/12">{detail.item}</div>
+					<div className="list-col-grow hidden justify-end sm:flex">
+						<OverlappingAvatars users={detail.users} />
 					</div>
-					{/* TODO: Add in once you can test with multiple avatars. <div><Avatars users={detail.users} /></div> */}
-					<div>
+					<div className="text-right text-sm">
 						{detail.totalCommitments} of {detail.requestedCount} filled
 					</div>
 				</li>
