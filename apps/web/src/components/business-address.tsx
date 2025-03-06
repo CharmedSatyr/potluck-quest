66ed7abcae9bd8fe6150f028bb8db4399c92616b
Joseph Wolfe <joseph@WLF.to>
2025-03-06 12:29:39 -0800

    Run prettier

--- apps/web/src/components/business-address.tsx
+++ apps/web/src/components/business-address.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 const BusinessAddress = () => {
-    const address = "62189 AW ,elttaeS ,WS evA ht03 6357".split("").reverse().join("")
+	const address = "62189 AW ,elttaeS ,WS evA ht03 6357"
+		.split("")
+		.reverse()
+		.join("");
 
-    return <span>{address}</span>
-}
+	return <span>{address}</span>;
+};
 
 export default BusinessAddress;
