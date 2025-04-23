dc995f2826a1a204c84d0c9ceeddb6d6d45075ec
Joseph Wolfe <joseph@WLF.to>
2025-04-23 13:52:47 -0700

    Upgrade to @neondatabase/serverless v1

--- apps/web/jest.setup.ts
+++ apps/web/jest.setup.ts
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+import {
+	TextDecoder as NodeTextDecoder,
+	TextEncoder as NodeTextEncoder,
+} from "util";
+
+if (typeof global.TextDecoder === "undefined") {
+	global.TextDecoder = NodeTextDecoder as typeof TextDecoder;
+}
+
+if (typeof global.TextEncoder === "undefined") {
+	global.TextEncoder = NodeTextEncoder as typeof TextEncoder;
+}
